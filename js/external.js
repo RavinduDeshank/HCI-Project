cb31d3d1feb3bc9abe1f7b0cd18181446c6a29e0
Ravindu Nethsara <ravindu15nethsara@gmail.com>
2020-10-16 15:39:00 +0530

    JS files updated

---
+++ js/external.js
@@ -0,0 +1,127 @@
+function validateForm(){
+    var x = document.forms["myForm"]["name-with-initials"].value;
+    if(x == ""){
+    alert("Name must be filled out with initials");
+    return false;
+    }
+  }
+
+  function validateForm(){
+    var x = document.forms["myForm"]["full-name"].value;
+    if(x == ""){
+    alert("Full Name must be filled out");
+    return false;
+    }
+  }
+
+  function validateForm(){
+    var x = document.forms["myForm"]["birthday"].value;
+    if(x == ""){
+    alert("Birthday must be filled out");
+    return false;
+    }
+  }
+
+  function validateForm(){
+    var x = document.forms["myForm"]["email"].value;
+    if(x == ""){
+    alert("Email must be filled out");
+    return false;
+    }
+  }
+
+  function validateForm(){
+    var x = document.forms["myForm"]["address"].value;
+    if(x == ""){
+    alert("Address must be filled out");
+    return false;
+    }
+  }
+
+  function validateForm(){
+    var x = document.forms["myForm"]["m_number"].value;
+    if(x == ""){
+    alert("Mobile Number must be filled out");
+    return false;
+    }
+  }
+
+  function validateForm(){
+    var x = document.forms["myForm"]["username"].value;
+    if(x == ""){
+    alert("Username must be filled out");
+    return false;
+    }
+  }
+
+  function validateForm(){
+    var x = document.forms["myForm"]["pass"].value;
+    if(x == ""){
+    alert("Password must be filled out");
+    return false;
+    }
+  }
+
+  function validateForm(){
+    var x = document.forms["myForm"]["designation"].value;
+    if(x == ""){
+    alert("Designation must be filled out");
+    return false;
+    }
+  }
+
+  function validateForm(){
+    var x = document.forms["myForm"]["area"].value;
+    if(x == ""){
+    alert("Area must be filled out");
+    return false;
+    }
+  }
+  
+  function validateForm(){
+    var x = document.forms["myForm"]["work-place"].value;
+    if(x == ""){
+    alert("Work Place must be filled out");
+    return false;
+    }
+  }
+
+  function validateForm(){
+    var x = document.forms["myForm"]["o-address"].value;
+    if(x == ""){
+    alert("Office Address must be filled out");
+    return false;
+    }
+  }
+
+  function validateForm(){
+    var x = document.forms["myForm"]["o-tp"].value;
+    if(x == ""){
+    alert("Office Telephone must be filled out");
+    return false;
+    }
+  }
+
+  function validateForm(){
+    var x = document.forms["myForm"]["h-qualification"].value;
+    if(x == ""){
+    alert("Highest Qualification must be filled out");
+    return false;
+    }
+  }
+
+  function validateForm(){
+    var x = document.forms["myForm"]["qualification"].value;
+    if(x == ""){
+    alert("Qualifications must be filled out");
+    return false;
+    }
+  }
+
+  function validateForm(){
+    var x = document.forms["myForm"]["s-areas"].value;
+    if(x == ""){
+    alert("Sepcialized areas must be filled out");
+    return false;
+    }
+  }
